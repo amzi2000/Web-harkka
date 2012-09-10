a0d8e06aeae65d3786fe178d04c37a3b883bb70f
Viorica Raduly-Baka <viorica.raduly.baka@gmail.com>
2012-09-10 15:34:40 +0300

    navigation improvement

---
+++ buttons.js
@@ -0,0 +1,18 @@
+function openListView() {
+	// open listView.html
+	window.location = "listView.html";
+}
+
+function createNewUser() {
+	// open listView.html
+	window.location = "userAdd.html";
+}
+
+function goToMain() {
+	// open listView.html
+	window.location = "index.html";
+}
+
+function addNewTask() {
+	window.location = "add_new_task.html";
+}
