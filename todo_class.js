2de000dd0241673927c14c97b05e17d653245ee8
amzi2000 <amzi2000@ovi.com>
2012-09-10 15:43:39 +0300

    Fixed array reference
    + other stuff

--- todo_class.js
+++ todo_class.js
@@ -3,12 +3,27 @@
  */
 
 var i;
+var Data = new Array();
+var mainIndex; 
+
+
+
+function list_item(index, taskName, taskDesc, status, responsible) {
+	this.taskIndex = index;
+	this.taskName = taskName;
+	this.taskDescription = taskDesc;
+	this.status = status;
+	this.responsible = responsible;
+
+	/* debug */
+//	document.write("new item");
+
+}
+
 
 function todo_list() {
 
-	var Data;
-
-	var mainIndex = 0; 
+	mainIndex = 0; 
 	
 	this.list_init = list_init;
 	this.list_draw = list_draw;
@@ -24,8 +39,7 @@ function todo_list() {
 function list_init() {
 	/* TBA, initialisation from file or server code here */
 	/* Currently always creates an empty list */
-	this.Data = undefined;
-	this.mainIndex = 0;
+	mainIndex = 0;
 
 	/* debug */
 //	document.write("init");
@@ -33,22 +47,25 @@ function list_init() {
 }
 
 function list_add(name, description, status, responsible) {
-	this.Data[mainIndex] = new list_item(mainIndex, name, description, status, responsible);
-	this.mainIndex = this.mainIndex + 1;
+
+
+    Data[mainIndex] = new list_item(mainIndex, name, description, status, responsible);
+
+	mainIndex = mainIndex + 1;
 
 	/* debug */
-	//document.write("list add");
+//	document.write("list add");
 
 }
 
 function list_delete(index) {
-	for ( i = 0; i < this.mainIndex; i++) {
+	for ( i = 0; i < mainIndex; i++) {
 		if (i > index) {
 			/* remove reference to todo_list[index] */
-			this.Data[i - 1] = this.Data[i];
-			this.Data[i - 1].list_item.taskIndex = i - 1;
+			Data[i - 1] = Data[i];
+			Data[i - 1].taskIndex = i - 1;
 		}
-		this.mainIndex = this.mainIndex - 1;
+		mainIndex = mainIndex - 1;
 
 	}
 
@@ -59,18 +76,22 @@ function list_delete(index) {
 
 function list_draw() {
 
-	if (this.Data == undefined) {/* draw empty list */
+
+	if (mainIndex == 0) {/* draw empty list */
 
 		document.getElementById("listarea").innerHTML = "The list is empty"
 
 	} else {
 
 
-		for (i=0;i<this.mainIndex; i++)
+		for (i=0;i<mainIndex; i++)
 		{
-		document.getElementById("listarea").innerHTML = this.Data[i].list_item.taskName + this.Data[i].list_item.responsible + this.Data[i].list_item.status;			
+
+		document.getElementById("listarea").innerHTML = Data[i].taskName + "  " + Data[i].responsible + "   " + Data[i].status;			
+			
+//		document.getElementById("listarea").innerHTML = Data[i].list_item.taskName + Data[i].list_item.responsible + Data[i].list_item.status;			
 		}
-		
+
 
 	}
 
@@ -79,15 +100,4 @@ function list_draw() {
 
 }
 
-function list_item(index, taskName, taskDesc, status, responsible) {
-	this.taskIndex = index;
-	this.taskName = taskName;
-	this.taskDescription = taskDesc;
-	this.status = status;
-	this.responsible = responsible;
-
-	/* debug */
-	//document.write("new item");
-
-}
 
